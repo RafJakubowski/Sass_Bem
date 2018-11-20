1cb5379ba4c235963a09542da7f119f54b38bcd6
RafJakubowski <r.jakubowski.88@o2.pl>
2018-11-20 06:01:39 +0100

    com

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+const sass= require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('sass' , () =>{
-  return gulp.src('./app/sass/main.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass({outputStyle: 'compressed' }))
-  .pipe(autoprefixer({
-    browsers: ['last 2 version'],
-    cascade: false
-  }))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('./dist/css'))
+gulp.task('sass', () => {
+    return gulp.src('./app/sass/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('watch', () => {
-  gulp.watch('./app/sass/*/*.scss', ['sass'])
+    gulp.watch('./app/sass/*/*.scss', ['sass'])
 });
 
-gulp.task('imagemin', () =>{
-  gulp.src('./app/images/*.+(png|jpg)')
-  .pipe(imagemin())
-  .pipe(gulp.dest('.dist/images'))
+gulp.task('imagemin', () => {
+    gulp.src('./app/images/*.+(png|jpg)')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
 });
